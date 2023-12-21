1a82b16e40c910b440472af234c1ea5c2df32ad7
Quincy Jo <me@quincyjo.com>
2023-12-21 15:55:28 -0500

    Add ParseResult documentation.

--- src/main/scala/com/quincyjo/jsonpath/parser/ParseResult.scala
+++ src/main/scala/com/quincyjo/jsonpath/parser/ParseResult.scala
@@ -4,54 +4,77 @@ import cats.{Applicative, Eval, Monad, Traverse}
 import com.quincyjo.jsonpath.parser.JsonPathParser.Token
 import scala.util.control.NoStackTrace
 
+/** Models a parsed value which may have failed.
+  * @tparam T The type that was parsed.
+  */
 sealed trait ParseResult[+T] {
 
+  /** Filter this result based on the given predicate, resulting in the provided orElse if the predicate is false.
+    * @param predicate Filter to apply to this result value.
+    * @param orElse Value to return if the predicate is false.
+    * @tparam B The type of the value to return if the predicate is false.
+    * @return This result if it passes the predicate, otherwise the provided orElse.
+    */
   def filterOrElse[B >: T](
-    predicate: T => Boolean,
-    orElse: => ParseResult[B]
+      predicate: T => Boolean,
+      orElse: => ParseResult[B]
   ): ParseResult[B]
 
+  /** Returns this result value if this is a success, otherwise the provided default.
+    * @param default Value to return if this result is a failure.
+    * @tparam B The type of the value to return if this result is a failure.
+    * @return This result if this is a success, otherwise the provided default.
+    */
   def getOrElse[B >: T](default: => B): B
-  
+
+  /** If this result is a success, return the value, otherwise throw the error.
+    * @throws ParseError If this result is a [[ParseError]].
+    * @return The value if this result is a success.
+    */
+  @throws[ParseError]
   def get: T
 }
 
 final case class Parsed[T](value: T) extends ParseResult[T] {
 
   override def filterOrElse[B >: T](
-    predicate: T => Boolean,
-    orElse: => ParseResult[B]
+      predicate: T => Boolean,
+      orElse: => ParseResult[B]
   ): ParseResult[B] =
     if (predicate(value)) this else orElse
 
   override def getOrElse[B >: T](default: => B): B = value
 
+  @throws[ParseError]
   override def get: T = value
 }
 
 final case class ParseError(message: String, index: Int, input: String)
-    extends Throwable(s"Failed to parse JsonPath due to '$message' at index $index in '$input'")
+    extends Throwable(
+      s"Failed to parse JsonPath due to '$message' at index $index in '$input'"
+    )
     with NoStackTrace
     with ParseResult[Nothing] {
 
   override def filterOrElse[B >: Nothing](
-    predicate: Nothing => Boolean,
-    orElse: => ParseResult[B]
+      predicate: Nothing => Boolean,
+      orElse: => ParseResult[B]
   ): ParseResult[B] =
     this
 
   override def getOrElse[B >: Nothing](default: => B): B = default
-  
+
+  @throws[ParseError]
   override def get: Nothing = throw this
 }
 
 object ParseError {
 
   def invalidToken(
-    invalidToken: Token,
-    i: Int,
-    input: String,
-    validTokens: Token*
+      invalidToken: Token,
+      i: Int,
+      input: String,
+      validTokens: Token*
   ): ParseError =
     new ParseError(
       s"Invalid token $invalidToken at index $i, expected one of: ${validTokens.mkString(", ")}",
@@ -63,12 +86,13 @@ object ParseError {
 
 object ParseResult {
 
-  implicit val monad: Monad[ParseResult] = new Monad[ParseResult] with Traverse[ParseResult] {
+  implicit val monad: Monad[ParseResult] = new Monad[ParseResult]
+    with Traverse[ParseResult] {
     override def pure[A](x: A): ParseResult[A] =
       Parsed(x)
 
     override def flatMap[A, B](
-      fa: ParseResult[A]
+        fa: ParseResult[A]
     )(f: A => ParseResult[B]): ParseResult[B] =
       fa match {
         case Parsed(value)     => f(value)
@@ -77,7 +101,7 @@ object ParseResult {
 
     @annotation.tailrec
     override def tailRecM[A, B](
-      a: A
+        a: A
     )(f: A => ParseResult[Either[A, B]]): ParseResult[B] = f(a) match {
       case failure: ParseError => failure
       case Parsed(Right(b))    => Parsed(b)
@@ -85,7 +109,7 @@ object ParseResult {
     }
 
     override def traverse[G[_]: Applicative, A, B](
-      fa: ParseResult[A]
+        fa: ParseResult[A]
     )(f: A => G[B]): G[ParseResult[B]] =
       fa match {
         case Parsed(a) =>
@@ -101,7 +125,7 @@ object ParseResult {
       }
 
     override def foldRight[A, B](fa: ParseResult[A], lb: Eval[B])(
-      f: (A, Eval[B]) => Eval[B]
+        f: (A, Eval[B]) => Eval[B]
     ): Eval[B] =
       fa match {
         case Parsed(value)     => f(value, lb)
